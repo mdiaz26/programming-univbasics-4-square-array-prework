3dd79695d75ba23b49b9cfd37582c00e7cd1a7fd
Michael Diaz <mike.diaz006@gmail.com>
2019-12-11 14:42:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
 count = 0 
 
 while count < array.length do
-  squared_array [count] = array[count] ** 2 
+  squared_array[count] = array[count] ** 2 
   count +=1 
 end
 squared_array
